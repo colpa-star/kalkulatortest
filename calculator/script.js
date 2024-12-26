e9ac849274d45e703c83b08dd5a646f26fc095ec
colpa-star <acolpa539@gmail.com>
2024-12-26 15:43:44 +0400

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,20 @@
+// script.js
+
+function appendInput(value) {
+    const resultField = document.getElementById('result');
+    resultField.value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculateResult() {
+    const resultField = document.getElementById('result');
+    try {
+      resultField.value = eval(resultField.value);
+    } catch (error) {
+      resultField.value = 'Error';
+    }
+  }
+  
